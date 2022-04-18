1c5f91c220fbe2ca4f2e87a0fa94f6c05610ed4f
Erwin Alam <djancok901@gmail.com>
2022-04-18 15:00:53 +0700

    update refferal

--- src/components/Refferal/Refferal.js
+++ src/components/Refferal/Refferal.js
@@ -1,13 +1,41 @@
-import { Card } from "antd";
+import {useEffect, useState} from "react"
+import { Input,Tooltip,Card,Button } from "antd";
+import { CopyOutlined } from '@ant-design/icons';
+
+import {useMoralis} from 'react-moralis'
+import Title from "antd/lib/typography/Title";
+
+
+
 
 
 function Refferal(){
+    const { isAuthenticated, account,user } = useMoralis();
+    const [refferalId,setRefferalId] = useState()
+
+    const generateId = ()=>{
+
+    }
+
+    useEffect(()=>{
+        if(isAuthenticated){
+            user.attributes.refferal ? setRefferalId(user.attributes.refferal) : setRefferalId(null)
+        }
+    },[user,isAuthenticated])
+
     return (
         <div className="site-card-border-less-wrapper">
-            <Card title="Card title" bordered={false} style={{ width: 300 }}>
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
+            <Card title="Refferal Program" bordered={false} style={{ width: 300 }}>
+            <Title level={5}>Your Id Refferal</Title>
+            <Input.Group compact>
+                <Input
+                    style={{ width: 'calc(100% - 50px)' }}
+                    defaultValue={refferalId}
+                />
+                <Tooltip title="copy refferal ID">
+                    <Button icon={<CopyOutlined />} />
+                </Tooltip>
+            </Input.Group>
             </Card>
         </div>
     )
